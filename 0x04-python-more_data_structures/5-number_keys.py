313c78b011413931ff6824e09c7927219cbd29d3
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-07-06 11:05:28 +0300

    A function that returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """A function that returns the number of keys present in a dictionary"""
+    if a_dictionary is not None:
+        return (len(a_dictionary))

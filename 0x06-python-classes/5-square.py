95d45e7a8d81ad685ecc8e7deed7bc8f3464fc0e
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-07-21 13:59:27 +0300

    Added a Public instance method def my_print(self): that prints the square with character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square.Getter method"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """The setter method fot the private aatribute size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """prints the square with the # character"""
+        for row in range(0, self.__size):
+            for column in range(self.__size):
+                print('#', end='')
+            print("")
+        if self.__size == 0:
+            print("")

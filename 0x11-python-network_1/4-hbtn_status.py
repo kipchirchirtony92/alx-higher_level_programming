6b8ba0fd08666425cf78f2018168a1f7a6f697e7
kipchirchirtony92 <kipchirchirtony92@gmail.com>
2022-10-23 12:04:24 +0300

    a Python script that fetches https://alx-intranet.hbtn.io/status using the package requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Fetches https://alx-intranet.hbtn.io/status using requests pkg"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))

dea59c490183d4fb60bf2045c82ce381cf413a0f
Tony Kipchirchir <kipchirchirtony92@gmail.com>
2023-12-18 13:51:42 +0000

    adding a static method that draws on the screen using turtle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 
 """ Defines the class model class """
 import json
+import csv
+import turtle
 
 class Base:
     """
@@ -75,3 +77,79 @@ class Base:
                     return instance_list
         except FileNotFoundError:
             return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        filename = cls.__name__ + ".csv"
+
+        with open(filename, mode='w', newline="") as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    row = [obj.id, obj.width, obj.height, obj.x, obj.y]
+                elif cls.__name__ == "Square":
+                    row = [obj.id, obj.size, obj.x, obj.y]
+                else:
+                    raise ValueError("Ivalid class for csv serailization")
+                writer.writerow(row)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserialize the instances from the CSV return a list of instances loaded from a file"""
+        filename = cls.__name__+ ".csv"
+        instance_list = []
+
+        try:
+            with open(filename, mode='r', newline="") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        instance = cls(int(row[0]), int(row[1]), int(row[2]), int(row[3]), int(row[4]))
+                    elif cls.__name__ == "Square":
+                        instance = cls(int(row[0]), int(row[1]), int(row[2]), int(row[3]))
+                    else:
+                        raise ValueError("Invalid class type for csv deserialization")
+                instance_list.append(instance)
+        except FileNotFoundError:
+            pass  # If the file doesn't exist, return an empty list
+        return instance_list
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Draw rectangles and squares using Turtle graphics.
+
+        Args:
+            list_rectangles (list): A list of Rectangle instances.
+            list_squares (list): A list of Square instances.
+
+        Returns:
+            None
+        """
+        screen = turtle.Screen()
+        screen.bgcolor("white")
+        screen.title("Draw Rectangles and Squares")
+
+        t = turtle.Turtle()
+        t.speed(2)
+
+        for rectangle in list_rectangles:
+            t.penup()
+            t.goto(rectangle.x, rectangle.y)
+            t.pendown()
+            t.color("blue")
+            for _ in range(2):
+                t.forward(rectangle.width)
+                t.left(90)
+                t.forward(rectangle.height)
+                t.left(90)
+
+        for square in list_squares:
+            t.penup()
+            t.goto(square.x, square.y)
+            t.pendown()
+            t.color("red")
+            for _ in range(4):
+                t.forward(square.size)
+                t.left(90)
+
+        turtle.done()

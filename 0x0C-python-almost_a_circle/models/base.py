e554e44ed528be416caaee5aeb82ecc6ecc8d170
Tony Kipchirchir <kipchirchirtony92@gmail.com>
2023-12-18 12:41:30 +0000

    returns an instance with default values of all the inherited class of base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,3 +50,14 @@ class Base:
             return []
         return json.loads(json_string)
 
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            dummy_instance = cls()
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+    
